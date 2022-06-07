c52f5ea29a9ecacf7448f11e93cdabedcf165a3c
綾乃 <>
2022-06-07 22:19:35 +0900

    [create]profileコントローラー

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,14 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @profile = current_user.profile
+  end
+
+  def edit
+  end
+
+  def update
+    
+  end
+end
