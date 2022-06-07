3b4722b669b17d2baec22ffdb93d7144f70ec6a2
綾乃 <>
2022-06-07 22:12:07 +0900

    モデルのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true, length: {maximum: 50}
+  has_one :profile, dependent: :destroy
 end
