fa8fcfa207370197d0a25eb5ffbbea91d5bce5f1
綾乃 <>
2022-06-08 12:11:03 +0900

    actiontext使用のため既存のカラム削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_rich_text :content
   validates :title, presence: true
   validates :title ,length: {minimum:2 ,maximum:50 }
   validates :title,format: { with: /\A(?!\@)/}
 
-  validates :content, presence: true
   validates :subcontent, presence: true
 
   def get_image(width, height)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_030250) do
+ActiveRecord::Schema.define(version: 2022_06_08_030950) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 2022_06_08_030250) do
   create_table "articles", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
-    t.text "content", null: false
     t.text "subcontent", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
