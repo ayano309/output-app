e7f893d051f7d90c9aed35e549f287473e79ce1b
綾乃 <>
2022-06-08 20:24:50 +0900

    アカウントページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy] 
   end
+
+  resources :accounts, only: [:show] do
+    #フォローする、外す(いいね関係と同じでresourceに注意)
+    resource :relationships, only: [:create, :destroy]
+    #フォローした人一覧
+    get 'followings' => 'relationships#followings', as: 'followings'
+    #フォローされた人一覧
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
     
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
