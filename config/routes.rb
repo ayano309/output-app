5943190f67bbd0a986a4b27fafab96de642ed283
綾乃 <>
2022-06-08 10:52:27 +0900

    [create]article-model,controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/edit'
   devise_for :users
   root to: "home#index"
   resource :profile, only: [:show, :edit, :update]
