70efde5a39651baa445773e739db2115ac27f31b
綾乃 <>
2022-06-08 20:14:45 +0900

    relationshipモデルを使う

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,32 @@ class User < ApplicationRecord
   has_many :articles,dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  #フォローの関係(２行目は一覧画面で使う)自分がフォローする
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  #フォロワーの関係(２行目は一覧画面で使う)自分がフォローされる
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  # フォローしたときの処理
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  # フォローを外すときの処理
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
+  end
+
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+    #following_relationships.exists?(following_id: user.id)
+  end
+
   #profileあれば更新、なければ作成
   def prepare_profile
     profile || build_profile
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy] 
   end
-  
+    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
