fbfef73e6426a006b600d363c43f641ecac1836a
綾乃 <>
2022-06-08 12:07:45 +0900

    action-text-install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,6 @@ import '@fortawesome/fontawesome-free/js/all'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,12 @@ class Article < ApplicationRecord
 
   validates :content, presence: true
   validates :subcontent, presence: true
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
