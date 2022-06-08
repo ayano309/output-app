09893cc85aada756ded7185ce8c80945d07afe91
綾乃 <>
2022-06-08 22:08:46 +0900

    relationshipコントローラーのアクション

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,28 @@
 class RelationshipsController < ApplicationController
-  def followings
+  before_action :authenticate_user!
+  # フォローするとき
+  def create
+    user = User.find(params[:user_id])
+    current_user.follow!(user)
+	redirect_to request.referer
   end
 
+  # フォロー外すとき
+  def destroy
+    user = User.find(params[:user_id])
+    current_user.unfollow!(user)
+    redirect_to request.referer
+  end
+
+  # フォロー一覧
+  def followings
+    user = User.find(params[:user_id])
+	@users = user.followings
+  end
+
+  # フォロワー一覧
   def followers
+    user = User.find(params[:user_id])
+	@users = user.followers
   end
 end
