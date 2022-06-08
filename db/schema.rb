048e9d486b229ee05d15313ac6d72ec957858a5f
綾乃 <>
2022-06-08 11:15:48 +0900

    [create]article-indexページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_130811) do
+ActiveRecord::Schema.define(version: 2022_06_08_014746) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
+    t.text "subcontent", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id", null: false
