97f01ffc8e7854873d34b1079239166ead1fb2da
綾乃 <>
2022-06-11 17:25:47 +0900

    group_userコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   #ダッシュボード
   resource :dashboard, only: [:show]
   #グループ
-  resources :groups
+  resources :groups do
+    resource :group_users, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
