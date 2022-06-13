923676ce1f26447cc80597c318a32fca93661982
綾乃 <>
2022-06-13 17:34:35 +0900

    [add]favorite,comment,relationshipモデルに通知メソッド記述

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,12 @@ class FavoritesController < ApplicationController
     @article = Article.find(params[:article_id])
     @favorite = current_user.favorites.new(article_id: @article.id)
     @favorite.save
-
+    #通知の作成
+    @article.create_notification_by(current_user)
+    respond_to do |format|
+      format.html {redirect_to request.referrer}
+      format.js
+    end
   end
 
   def destroy
