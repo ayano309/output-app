47a634d87841055a5dc6d586f075bc05e5120560
綾乃 <>
2022-06-14 11:21:58 +0900

    mailコントローラーアクション追加

--- app/controllers/event_notices_controller.rb
+++ app/controllers/event_notices_controller.rb
@@ -1,4 +1,32 @@
 class EventNoticesController < ApplicationController
+  #メール新規フォーム
   def new
+    @group = Group.find(params[:group_id])
+  end
+  
+  #フォームの内容を受け取る
+  def create
+    #フォームからgroup_idとtitle,bodyを受け取る
+    @group = Group.find(params[:group_id])
+    @title = params[:title]
+    @body = params[:body] 
+
+    #受け取った値をハッシュに
+    event = { 
+      :group => @group, 
+      :title => @title, 
+      :body => @body
+      
+    }
+    
+    #EventMailer.rbにハッシュを送る
+    EventMailer.send_notifications_to_group(event)
+    #送信後の確認画面へ
+    render :sent
+  end
+  
+  #groupの詳細画面へリダイレクト
+  def sent
+    redirect_to group_path(params[:group_id])
   end
 end
