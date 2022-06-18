35754f33d91934498161cf4daeced0ada4ab8720
綾乃 <>
2022-06-18 13:00:16 +0900

    [add]spec-factoriesファイル

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :group do
-    
+    association :user
+    name { Faker::Lorem.characters(number: 5) }
+    introduction { Faker::Lorem.characters(number: 20) }
   end
 end
