8974d38be4340f3b1bae0a5ad4ee5c6dc635273b
綾乃 <>
2022-06-27 22:29:50 +0900

    [add]timelineでページネーション

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,6 +2,6 @@ class TimelinesController < ApplicationController
   before_action :authenticate_user!
   def show
     user_ids = current_user.followings.pluck(:id)
-    @articles = Article.preload(:user).where(user_id:user_ids)
+    @articles = Article.preload(:user).where(user_id:user_ids).display_list(params[:page])
   end
 end
