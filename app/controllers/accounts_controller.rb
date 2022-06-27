9d3b1624a7c2c4395aede2730bc182296730ddbb
綾乃 <>
2022-06-27 23:15:48 +0900

    [add]User検索機能

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,15 @@
 class AccountsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @users = User.eager_load(profile: {avatar_attachment: :blob}).display_list(params[:page])
+    if params[:keyword].present?
+      @keyword = params[:keyword].strip
+      @users = User.search_information(@keyword).display_list(params[:pages])
+    else
+      @keyword = ""
+      @users = User.eager_load(profile: {avatar_attachment: :blob}).display_list(params[:page])
+    end
   end
+  
   def show
     @user = User.find(params[:id])
       if @user == current_user
