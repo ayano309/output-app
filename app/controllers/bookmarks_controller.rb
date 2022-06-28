0e21341087a101250f5339fe797e29a25febf6b6
綾乃 <>
2022-06-28 22:25:53 +0900

    [add]bookmark機能、非同期化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,14 @@
 class BookmarksController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @article = Article.find(params[:article_id])
+    @bookmark = current_user.bookmarks.new(article_id: @article.id)
+    @bookmark.save
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @bookmark = current_user.bookmarks.find_by(article_id: @article.id)
+    @bookmark.destroy
+  end
 end
